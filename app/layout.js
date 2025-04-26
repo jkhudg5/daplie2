f05180566108f72ab8fb66d5168b7def751d8596
jkhudg5 <popokathepetel15@web.de>
2025-04-26 12:58:12 +0700

    Add files via upload

---
+++ app/layout.js
@@ -0,0 +1,37 @@
+import './globals.css'
+import { Inter } from 'next/font/google'
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Daplie',
+  description: 'Daplie - Ihre Experten für KI, Softwareentwicklung und Linkbuilding',
+  icons: {
+    icon: '/images/e0994a6c-c4c4-4fc3-ac8b-51ef97e09396.png'
+  }
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="de">
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
+          integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg=="
+          crossOrigin="anonymous"
+          referrerPolicy="no-referrer"
+        />
+      </head>
+      <body className={inter.className}>
+        <div className="flex flex-col min-h-screen">
+          <Header />
+          <main className="flex-grow">{children}</main>
+          <Footer />
+        </div>
+      </body>
+    </html>
+  )
+} 
